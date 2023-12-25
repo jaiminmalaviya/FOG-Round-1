bea1f522547ba4c35e372504c49d1ba6cf9e5945
Jaimin Malaviya <Jaiminmalviya56.jm@gmail.com>
2023-12-26 01:42:18 +0530

    backend data change

--- backend/multiplayerData.js
+++ backend/multiplayerData.js
@@ -2,22 +2,22 @@ const multiplayerData = [
    {
       title: 'QUICKMATCH',
       data: 'The quickest way to get into any of the game modes.',
-      image: 'QUICKMATCH', // Replace with the actual image filename or URL
+      image: 'QUICK_MATCH', // Replace with the actual image filename or URL
    },
    {
       title: 'SQUAD JOIN',
       data: 'Play with some friends.',
-      image: 'SQUADJOIN', // Replace with the actual image filename or URL
+      image: 'SQUAD_JOIN', // Replace with the actual image filename or URL
    },
    {
       title: 'SERVER BROWSER',
       data: 'The old school way of finding that perfect game. Set up your filters and you’re ready to go.',
-      image: 'SERVERBROWSER', // Replace with the actual image filename or URL
+      image: 'SERVER_BROWSER', // Replace with the actual image filename or URL
    },
    {
       title: 'TEST RANGE',
       data: 'Try out all the weapons, gadgets, and vehicles to improve your skills on the battlefield.',
-      image: 'TESTRANGE', // Replace with the actual image filename or URL
+      image: 'TEST_RANGE', // Replace with the actual image filename or URL
    },
 ]
 
