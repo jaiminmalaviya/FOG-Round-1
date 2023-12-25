523cc6f0a7ec6de2186e748be9f3b4d511fa5430
Jaimin Malaviya <Jaiminmalviya56.jm@gmail.com>
2023-12-26 01:42:18 +0530

    backend data change

--- frontend/src/components/Multiplayer.jsx
+++ frontend/src/components/Multiplayer.jsx
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react'
-import QUICKMATCH from '../assets/multiplayer/1.png'
-import SQUADJOIN from '../assets/multiplayer/2.png'
-import SERVERBROWSER from '../assets/multiplayer/3.png'
-import TESTRANGE from '../assets/multiplayer/4.png'
+import QUICK_MATCH from '../assets/multiplayer/1.png'
+import SQUAD_JOIN from '../assets/multiplayer/2.png'
+import SERVER_BROWSER from '../assets/multiplayer/3.png'
+import TEST_RANGE from '../assets/multiplayer/4.png'
 
 const imageMap = {
-   QUICKMATCH,
-   SQUADJOIN,
-   SERVERBROWSER,
-   TESTRANGE,
+   QUICK_MATCH,
+   SQUAD_JOIN,
+   SERVER_BROWSER,
+   TEST_RANGE,
 }
 
 const Multiplayer = () => {
@@ -17,7 +17,7 @@ const Multiplayer = () => {
    useEffect(() => {
       const fetchData = async () => {
          try {
-            const response = await fetch('http://localhost:5000/api/multiplayer')
+            const response = await fetch('https://fog-round-1-backend.vercel.app/api/multiplayer')
             const data = await response.json()
             setMultiplayerData(data.data)
          } catch (error) {
@@ -33,28 +33,31 @@ const Multiplayer = () => {
    }
 
    return (
-      <div className="relative mt-24 flex gap-x-1">
+      <div className="relative mt-16 flex gap-x-1 flex-wrap">
          {multiplayerData.map((option, index) => (
             <div
                key={index}
-               className="w-[210px] h-[294px]"
+               className="group w-[210px] h-[294px] flex flex-col overflow-hidden"
             >
-               <div className="h-[174px] relative">
-                  <div className="bg-[#101010]/30 w-full h-full absolute"></div>
+               <div className="h-[174px] relative overflow-hidden">
+                  <div className="bg-[#101010]/30 w-full h-full absolute z-10"></div>
                   <img
                      src={imageMap[option.image]}
                      alt={`${option.title} Image`}
+                     className="h-[182px] group-hover:-translate-y-2 transition-transform"
                   />
                </div>
-               <div className="mt-4 px-[14px]">
-                  <h3 className="text-white text-[28px] font-bold  uppercase tracking-wide">
+               <div className="pt-4 px-4 flex-grow bg-[#101010]/50 group-hover:bg-white group-hover:text-black transition-all z-10">
+                  <h3 className="text-xl font-bold uppercase tracking-wider mb-1">
                      {option.title}
                   </h3>
-                  <p>{option.data}</p>
+                  <p className="opacity-50 text-xs font-semibold pr-4 tracking-wide">
+                     {option.data}
+                  </p>
                </div>
             </div>
          ))}
-      </div>
+      </div>q
    )
 }
 
